87bad4849e60ebae2243b8ad844cc142804cee96
TwoStarLightMints <mondo.beyondo3@gmail.com>
2022-09-16 22:42:19 -0400

    Implement functionality to getComputerChoice

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 function getComputerChoice () {
   // Randomly returns either 'Rock', 'Paper', or 'Scissors'
+  let choice = Math.floor(Math.random() * 3);
+  let choices = ['Rock', 'Paper', 'Scissors'];
+  return choices[choice];
 }
+
+console.log(getComputerChoice());
