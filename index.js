776599057e58b92d24228814abda738570f96d0c
TwoStarLightMints <mondo.beyondo3@gmail.com>
2022-09-18 00:15:24 -0400

    Create query selector to update h1 when game ends

--- index.js
+++ index.js
@@ -70,6 +70,8 @@ function game () {
   for (let i = 0; i < 5; i++) {
     console.log(playRound(getCleanInput(), getComputerChoice()));
   }
+
+  document.getElementById("tag").innerHTML = "Thanks for playing!";
 }
 
 game();
